1d180adecfda792ca7d8c66f4c88c25f841aa400
magicwebkraken <terryvenusdev520@gmail.com>
2025-12-24 03:05:50 -0800

    sdfsdf

---
+++ _next/static/JzK9a6EHeFNKTXSR-QXnH/_buildManifest.js
@@ -0,0 +1,20 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/26b55bd98df1f30f.js"
+  ],
+  "/_error": [
+    "static/chunks/6356b693f9834142.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/hello",
+    "/api/price"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
